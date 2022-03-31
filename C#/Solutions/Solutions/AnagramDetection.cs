754f9beaed4f16f8d767281cdb2a314f8b4c5bc8
neighborpil <neighborpil@naver.com>
2022-03-31 20:20:15 +0900

    settup solution project

---
+++ C#/Solutions/Solutions/AnagramDetection.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Solutions
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ C#/Solutions/Solutions/UnitTest1.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+
+namespace Solutions
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class AnagramDetection
+    {
+        
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
